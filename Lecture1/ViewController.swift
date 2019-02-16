da9c6a515bae666183c254e7bf0b0585dfd133c4
elifyurt <yrtelf@gmail.com>
2019-02-16 13:32:58 +0300

    Initial Commit

---
+++ Lecture1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Lecture1
+//
+//  Created by Elif Yurt on 16.02.2019.
+//  Copyright © 2019 Elif Yurt. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
